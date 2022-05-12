3c255bf8a10572f7dc55d1b94c337a73e9167285
CodyMurr <codakmurraki@gmail.com>
2022-05-12 00:30:59 -0500

    update

--- src/components/DataDisplay/DataDisplay.jsx
+++ src/components/DataDisplay/DataDisplay.jsx
@@ -1,36 +1,44 @@
-import { useContext, useState } from "react"
-import { capitalize } from "../../utilities/helper-functions";
+import './DataDisplay.css';
+import { useContext, useState } from 'react';
+import { capitalize } from '../../utilities/helper-functions';
 import Modal from 'react-modal';
-import TuningContext from "../../context/TuningContext";
-import Form from "../Form/Form";
+import TuningContext from '../../context/TuningContext';
+import Form from '../Form/Form';
 
 Modal.setAppElement('#root');
 
 export default function DataDisplay() {
-    const { signature } = useContext(TuningContext);
-    const sigCats = Object.keys(signature);
-    const [modalIsOpen, setIsOpen] = useState(false);
+	const { signature, content, switchContent } =
+		useContext(TuningContext);
+	const sigCats = Object.keys(signature);
+	const [modalIsOpen, setIsOpen] = useState(false);
 
-    function openModal() {
-        setIsOpen(true);
-    }
+	function openModal() {
+		setIsOpen(true);
+	}
 
-    function closeModal() {
-        setIsOpen(false)
-    }
+	function closeModal() {
+		setIsOpen(false);
+	}
 
-    return (
-    <div className="DataDisplay flex">
-        <Modal isOpen={modalIsOpen}>
-            <Form closeModal={closeModal}/>
-        </Modal>
-        {
-        sigCats.map(s => (
-            <section className="flex col" key={capitalize(s)}>
-                <strong>{capitalize(s)}:&nbsp;</strong>
-                <p>{capitalize(signature[s])}</p>
-            </section>
-        ))
-    }</div>
-    )
+	return (
+		<div className='DataDisplay flex col'>
+			<Modal isOpen={modalIsOpen}>
+				<Form closeModal={closeModal} />
+			</Modal>
+			{sigCats.map((s) => (
+				<section className='flex' key={capitalize(s)}>
+					<strong>{capitalize(s)}:&nbsp;</strong>
+					<p>{capitalize(signature[s])}</p>
+				</section>
+			))}
+			<div className='toggle-mode flex' onClick={switchContent}>
+				<span className={content === 1 && 'content'}>Scale</span>
+				<span className={content === -1 && 'content'}>Chords</span>
+			</div>
+			<div className='settings flex'>
+				<p onClick={openModal}>Go to Settings</p>
+			</div>
+		</div>
+	);
 }
--- src/context/TuningContext.jsx
+++ src/context/TuningContext.jsx
@@ -1,37 +1,55 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
 const TuningContext = createContext();
 
-export function TuningProvider({children}) {
-    const [signature, setSignature] = useState({
-        tuning: 'standard',
-        key: 'c',
-        progression: 'major',
-    });
-    const [formData, setFormData] = useState({
-        tuning: '',
-        key: '',
-        progression: '',
-    });
-    // if signature.progression is set to 'major'
-    const [mode, setMode] = useState('ionian');  // otherwise set to null
-    
-    function changeSig(newTuning, newKey, newProgression) {
-        setSignature({
-            ...signature,
-            tuning: newTuning,
-            key: newKey,
-            progression: newProgression
-        });
-    }
+export function TuningProvider({ children }) {
+	const [signature, setSignature] = useState({
+		tuning: 'standard',
+		key: 'c',
+		progression: 'major',
+	});
+	const [formData, setFormData] = useState({
+		tuning: '',
+		key: '',
+		progression: '',
+	});
+	// if signature.progression is set to 'major'
+	const [mode, setMode] = useState('ionian'); // otherwise set to null
+	// to toggle between rendering scale and chords
+	const [content, setContent] = useState(1); // options are 1 & -1
 
-    function changeMode(newMode) {
-        setMode(newMode);
-    }
+	function changeSig(newTuning, newKey, newProgression) {
+		setSignature({
+			...signature,
+			tuning: newTuning,
+			key: newKey,
+			progression: newProgression,
+		});
+	}
 
-    return <TuningContext.Provider value={{ signature, mode, formData, setFormData, changeSig, changeMode  }}>
-        {children}
-    </TuningContext.Provider>
+	function changeMode(newMode) {
+		setMode(newMode);
+	}
+
+	function switchContent() {
+		setContent(content * -1);
+	}
+
+	return (
+		<TuningContext.Provider
+			value={{
+				signature,
+				mode,
+				formData,
+				content,
+				setFormData,
+				changeSig,
+				changeMode,
+				switchContent,
+			}}>
+			{children}
+		</TuningContext.Provider>
+	);
 }
 
 export default TuningContext;
