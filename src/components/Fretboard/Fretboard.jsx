49fae3f336d48a671832435cb6f55ec21cd8e8f3
CodyMurr <codakmurraki@gmail.com>
2022-05-11 20:29:29 -0500

    render fretboard notes based on tuning

--- src/components/Fretboard/Fretboard.jsx
+++ src/components/Fretboard/Fretboard.jsx
@@ -1,26 +1,49 @@
-import './Fretboard.css'
+import './Fretboard.css';
 import { useContext } from 'react';
-import { tunings, fretStyles } from '../../utilities/service-logic';
+import {
+	tunings,
+	fretStyles,
+	getFretNotes,
+	getScale,
+} from '../../utilities/service-logic';
 import { capitalize } from '../../utilities/helper-functions';
 import TuningContext from '../../context/TuningContext';
 import Form from '../Form/Form';
 
 export default function Fretboard() {
-    const {signature} = useContext(TuningContext);
+	const { signature } = useContext(TuningContext);
 
-    const frets = Object.keys(fretStyles);
-    
-    return (
-    <div className='fb-container flex'>
-        <section className='tuner flex col'>
-            {
-                tunings[signature.tuning].map((note, idx) => <span className='note flex' key={idx}>{capitalize(note)}</span>)
-            }
-        </section> 
-        <section className="Fretboard flex">
-                {frets.map((group) => (
-                    fretStyles[group].frets.map((f, idx) => <section className=' fret flex col' key={`${f}.${idx}`} style={{ width: fretStyles[group].width}}></section>)
-                ))}
-        </section>
-    </div>)
+	const frets = Object.keys(fretStyles);
+
+	const fretNotes = getFretNotes(signature.tuning);
+
+	console.log(fretNotes);
+
+	return (
+		<div className='fb-container flex'>
+			<section className='tuner flex col'>
+				{tunings[signature.tuning].map((note, rootIdx) => (
+					<span className='note flex' key={rootIdx}>
+						{capitalize(note)}
+					</span>
+				))}
+			</section>
+			<section className='Fretboard flex'>
+				{frets.map((group) =>
+					fretStyles[group].frets.map((f, idx) => (
+						<section
+							className=' fret flex col'
+							key={`${f}.${idx}`}
+							style={{ width: fretStyles[group].width }}>
+							{tunings[signature.tuning].map((note, root) => (
+								<span className='note flex'>
+									{capitalize(fretNotes[root][f - 1])}
+								</span>
+							))}
+						</section>
+					)),
+				)}
+			</section>
+		</div>
+	);
 }
--- src/utilities/service-logic.js
+++ src/utilities/service-logic.js
@@ -1,67 +1,82 @@
 export const notes = [
-    "a",
-    "a#",
-    "b",
-    "c",
-    "c#",
-    "d",
-    "d#",
-    "e",
-    "f",
-    "f#",
-    "g",
-    "g#"
+	'a',
+	'a#',
+	'b',
+	'c',
+	'c#',
+	'd',
+	'd#',
+	'e',
+	'f',
+	'f#',
+	'g',
+	'g#',
 ];
 
 export const tunings = {
-    'standard': ['e', 'b', 'g', 'd', 'a', 'e'],
-    'half-step': ['d#', 'a#', 'f#', 'c#', 'g#', 'd#'],
-    'full-step': ['d', 'a', 'f', 'c', 'g', 'd'],
-    '1.5-steps': ['c#', 'g#', 'e', 'b', 'f#', 'c#'],
-    'drop-a': ['b', 'f#', 'd', 'a', 'e', 'a'],
-    'drop-b': ['c#', 'g#', 'e', 'b', 'f#', 'b'],
-    'drop-c': ['d', 'a', 'f', 'c', 'g', 'c'],
-    'drop-d': ['e', 'b', 'g', 'd', 'a', 'd'],
-    'drop-e': ['f#', 'c#', 'a', 'e', 'b', 'e'],
-    'drop-f': ['g', 'd', 'a#', 'f', 'c', 'f'],
-    'drop-g': ['a', 'e', 'c', 'g', 'd', 'g'],
-    'open-a': [ 'e', 'a', 'e', 'c#', 'a', 'e'],
-    'open-b': ['d#', 'b', 'f#', 'b', 'f#', 'b'],
-    'open-c': ['e', 'c', 'g', 'c', 'g', 'c'],
-    'open-d': ['d', 'a', 'f#', 'd', 'a', 'd'],
-    'open-e': ['e', 'b', 'g#', 'e', 'b', 'e'],
-    'open-f': ['f', 'a', 'f', 'c', 'f', 'c'],
-    'open-g': ['d', 'b', 'g', 'd', 'g', 'd'],
-}
+	standard: ['e', 'b', 'g', 'd', 'a', 'e'],
+	'half-step': ['d#', 'a#', 'f#', 'c#', 'g#', 'd#'],
+	'full-step': ['d', 'a', 'f', 'c', 'g', 'd'],
+	'1.5-steps': ['c#', 'g#', 'e', 'b', 'f#', 'c#'],
+	'drop-a': ['b', 'f#', 'd', 'a', 'e', 'a'],
+	'drop-b': ['c#', 'g#', 'e', 'b', 'f#', 'b'],
+	'drop-c': ['d', 'a', 'f', 'c', 'g', 'c'],
+	'drop-d': ['e', 'b', 'g', 'd', 'a', 'd'],
+	'drop-e': ['f#', 'c#', 'a', 'e', 'b', 'e'],
+	'drop-f': ['g', 'd', 'a#', 'f', 'c', 'f'],
+	'drop-g': ['a', 'e', 'c', 'g', 'd', 'g'],
+	'open-a': ['e', 'a', 'e', 'c#', 'a', 'e'],
+	'open-b': ['d#', 'b', 'f#', 'b', 'f#', 'b'],
+	'open-c': ['e', 'c', 'g', 'c', 'g', 'c'],
+	'open-d': ['d', 'a', 'f#', 'd', 'a', 'd'],
+	'open-e': ['e', 'b', 'g#', 'e', 'b', 'e'],
+	'open-f': ['f', 'a', 'f', 'c', 'f', 'c'],
+	'open-g': ['d', 'b', 'g', 'd', 'g', 'd'],
+};
 
 export const progressions = {
-    'major': [2, 2, 1, 2, 2, 2], 
-    'minor': [2, 1, 2, 2, 1, 2]
+	major: [2, 2, 1, 2, 2, 2],
+	minor: [2, 1, 2, 2, 1, 2],
+};
+
+export const fretStyles = {
+	group1: {
+		frets: [1, 2, 3, 4],
+		width: '10.5%',
+	},
+	group2: {
+		frets: [5, 6, 7, 8],
+		width: '8.5%',
+	},
+	group3: {
+		frets: [9, 10, 11, 12],
+		width: '6%',
+	},
+};
+
+export function baseNotes(root) {
+	let idx = notes.indexOf(root);
+	return notes.slice(idx + 1).concat(notes.slice(0, idx + 1));
 }
 
 export function getScale(root, prog) {
-    let rootIdx = notes.indexOf(root);
-    const noteArr = notes.slice(rootIdx).concat(notes.slice(0, rootIdx));
-    const scaleArr = [];
-    let idx = 0;
-    for (let i = 0; i <= progressions[prog].length; i++) {
-      scaleArr.push(noteArr[idx]);
-      idx += progressions[prog][i];
-    }
-    return scaleArr;
+	let rootIdx = notes.indexOf(root);
+	const noteArr = notes
+		.slice(rootIdx)
+		.concat(notes.slice(0, rootIdx));
+	const scaleArr = [];
+	let idx = 0;
+	for (let i = 0; i <= progressions[prog].length; i++) {
+		scaleArr.push(noteArr[idx]);
+		idx += progressions[prog][i];
+	}
+	return scaleArr;
 }
 
-export const fretStyles = {
-    group1: {
-        frets: [1, 2, 3, 4],
-        width: '10.5%'
-    },
-    group2:  {
-        frets: [5, 6, 7, 8],
-        width: '8.5%'
-    },
-    group3: {
-        frets: [9, 10, 11, 12],
-        width: '6%'
-    }
+export function getFretNotes(tuning) {
+	const fretArr = [];
+	tunings[tuning].forEach((root) => {
+		fretArr.push(baseNotes(root));
+	});
+	return fretArr;
 }
