13530782142cd51f8f18f660ebf0e15af6706475
CodyMurray-ga <codakmurraki@gmail.com>
2022-05-15 22:01:39 -0500

    render scale notes upon loading

--- src/context/TuningContext.jsx
+++ src/context/TuningContext.jsx
@@ -5,61 +5,64 @@ import { capitalize } from '../utilities/helper-functions';
 const TuningContext = createContext();
 
 export function TuningProvider({ children }) {
-	const [signature, setSignature] = useState({
-		tuning: 'standard',
-		key: 'c',
-		progression: 'major',
-	});
+  const [signature, setSignature] = useState({
+    tuning: 'standard',
+    key: 'c',
+    progression: 'major',
+  });
 
-	const [formData, setFormData] = useState({
-		tuning: '',
-		key: '',
-		progression: '',
-	});
+  const [formData, setFormData] = useState({
+    tuning: '',
+    key: '',
+    progression: '',
+  });
 
-	const [scale, setScale] = useState([]);
+  const [scale, setScale] = useState(
+    getScale(signature.key, signature.progression),
+  );
 
-	const [currentMode, setCurrentMode] = useState('scale');
+  const [currentMode, setCurrentMode] = useState('scale');
 
-	const display = {
-		tuning: <p>{tunings[signature.tuning].join('').toUpperCase()}</p>,
-		key: <p>{capitalize(signature.key)}</p>,
-		progression: <p>{capitalize(signature.progression)}</p>,
-	};
+  const display = {
+    tuning: <p>{tunings[signature.tuning].join('').toUpperCase()}</p>,
+    key: <p>{capitalize(signature.key)}</p>,
+    progression: <p>{capitalize(signature.progression)}</p>,
+  };
 
-	function changeSig(newTuning, newKey, newProgression) {
-		setSignature({
-			...signature,
-			tuning: newTuning,
-			key: newKey,
-			progression: newProgression,
-		});
-	}
+  function changeSig(newTuning, newKey, newProgression) {
+    setSignature({
+      ...signature,
+      tuning: newTuning,
+      key: newKey,
+      progression: newProgression,
+    });
+  }
 
-	function renderScale(newKey, prog) {
-		setScale(getScale(newKey, prog));
-	}
+  function renderScale(newKey, prog) {
+    setScale(getScale(newKey, prog));
+  }
 
-	function toggleMode(newMode) {
-		setCurrentMode(newMode);
-	}
+  function toggleMode(newMode) {
+    setCurrentMode(newMode);
+  }
 
-	return (
-		<TuningContext.Provider
-			value={{
-				signature,
-				formData,
-				scale,
-				currentMode,
-				display,
-				setFormData,
-				changeSig,
-				renderScale,
-				toggleMode,
-			}}>
-			{children}
-		</TuningContext.Provider>
-	);
+  return (
+    <TuningContext.Provider
+      value={{
+        signature,
+        formData,
+        scale,
+        currentMode,
+        display,
+        setFormData,
+        changeSig,
+        renderScale,
+        toggleMode,
+      }}
+    >
+      {children}
+    </TuningContext.Provider>
+  );
 }
 
 export default TuningContext;
